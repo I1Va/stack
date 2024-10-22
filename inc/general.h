18a8621b43c938623deaa79d440214c890e30614
I1Va <mai.abrutinid@gmil.com>
2024-10-22 17:00:41 +0300

    define fix

--- inc/general.h
+++ inc/general.h
@@ -39,7 +39,7 @@ enum RETURN_STATES {
     #define ON_DEBUG(...) __VA_ARGS__
     #define VERIFY(stk, return_err, exit_instruction)                                         \
         {                                                                                             \
-            if (verify(stk, return_err, __FILE__, __PRETTY_FUNCTION__, __LINE__) != stk_err_OK) { \
+            if (verify(stk, return_err, __FILE__, __PRETTY_FUNCTION__, __LINE__) != STK_ERR_OK) { \
                 exit_instruction;                                                                     \
             }                                                                                          \
         }
